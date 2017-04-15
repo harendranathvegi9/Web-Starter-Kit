514c61d72b683b91fe5a023c47330588ac9e324e
Shyam Chen <shyamchen1994@gmail.com>
2017-04-15 16:23:51 +0800

    component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,9 +3,13 @@ import { layout } from '../../components/layout';
 import template from './home.html';
 import data from './home.json';
 
+import { fileUpload } from '../../components/file-upload';
+
 export const home = () => {
   page('/', () => {
     layout(template(data));
+    fileUpload('example-1');
+    fileUpload('example-2', 'Choose a file');
     componentHandler.upgradeAllRegistered();
   });
 };
